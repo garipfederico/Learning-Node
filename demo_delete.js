c4fd3ba79270a7454f98f89239126da4d145daaa
garipfederico <garipfederico@gmail.com>
2021-02-05 10:40:20 -0300

    Delete Files

---
+++ demo_delete.js
@@ -0,0 +1,8 @@
+//The fs.unlink() method deletes the specified file:
+
+var fs = require('fs');
+
+fs.unlink('mynewfile3.txt', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+});
