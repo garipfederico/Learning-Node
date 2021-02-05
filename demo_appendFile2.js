02641347905150b88ce370b80df2d05fbd6235c8
garipfederico <garipfederico@gmail.com>
2021-02-05 10:29:23 -0300

    Update Files

---
+++ demo_appendFile2.js
@@ -0,0 +1,7 @@
+//The fs.appendFile() method appends the specified content at the end of the specified file:
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+});
---
+++ demo_writeFile2.js
@@ -0,0 +1,8 @@
+// The fs.writeFile() method replaces the specified file and content:
+
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+});
